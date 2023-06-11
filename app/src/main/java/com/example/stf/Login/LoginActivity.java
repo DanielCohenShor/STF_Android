efd1485f1c730ac19182c46acb58b7271f3fdec5
Daniel Cohen Shor <danielcohenshor@gmail.com>
2023-06-11 21:13:45 +0300

    fix login errors

--- app/src/main/java/com/example/stf/Login/LoginActivity.java
+++ app/src/main/java/com/example/stf/Login/LoginActivity.java
@@ -10,6 +10,10 @@ import android.os.Bundle;
 import android.content.Context;
 import android.graphics.drawable.Drawable;
 import android.text.InputType;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
 import android.util.TypedValue;
 import android.view.Gravity;
 import android.view.View;
@@ -50,31 +54,61 @@ public class LoginActivity extends AppCompatActivity {
     private void createListeners() {
         // i dont know for what ?
         Context context = this; // 'this' refers to the current Activity instance
-        btnPasswordVisibility.setOnClickListener(v -> {
+        btnPasswordVisibility.setOnClickListener(new View.OnClickListener() {
             boolean isPasswordVisible = false;
             final Drawable showPassword = ContextCompat.getDrawable(context, R.drawable.show_password_icon_drawable);
             final Drawable hidePassword = ContextCompat.getDrawable(context, R.drawable.hide_password_icon_drawable);
 
-            if (isPasswordVisible) {
-                btnPasswordVisibility.setImageDrawable(showPassword);
-                // Set the input type to hide the password
-                etPassword.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
-            } else {
-                btnPasswordVisibility.setImageDrawable(hidePassword);
-                // Set the input type to show the password
-                etPassword.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+            @Override
+            public void onClick(View v) {
+                if (isPasswordVisible) {
+                    btnPasswordVisibility.setImageDrawable(showPassword);
+                    // Set the input type to hide the password
+                    etPassword.setInputType(InputType.TYPE_CLASS_TEXT| InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                } else {
+                    btnPasswordVisibility.setImageDrawable(hidePassword);
+                    // Set the input type to show the password
+                    etPassword.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+                }
+                isPasswordVisible = !isPasswordVisible;
+
+                // Move the cursor to the end of the text
+                etPassword.setSelection(etPassword.getText().length());
             }
-            isPasswordVisible = !isPasswordVisible;
-
-            // Move the cursor to the end of the text
-            etPassword.setSelection(etPassword.getText().length());
         });
 
-        linkToRegister.setOnClickListener(v -> {
-            // Start the new activity here
-            Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
-            startActivity(intent);
-        });
+        // Define the text with the portion to be highlighted
+        String fullText = getResources().getString(R.string.tv_link_to_login);
+        String highlightText = "Click here";
+        int highlightStart = fullText.indexOf(highlightText);
+        int highlightEnd = highlightStart + highlightText.length();
+
+        // Create a SpannableString
+        SpannableString spannableString = new SpannableString(fullText);
+
+        // Create a ClickableSpan
+        ClickableSpan clickableSpan = new ClickableSpan() {
+            @Override
+            public void onClick(View widget) {
+                // Handle the click event here, e.g., change the color
+                int clickedColor = ContextCompat.getColor(LoginActivity.this, R.color.blue_shade_4);
+                ((TextView) widget).setLinkTextColor(clickedColor);
+
+                // Start the new activity here
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+
+                // Perform any other actions you want when the text is clicked
+                // For example, navigate to a new activity or perform some task.
+            }
+        };
+
+        // Apply the ClickableSpan to the desired portion of the text
+        spannableString.setSpan(clickableSpan, highlightStart, highlightEnd, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        // Make the text clickable and change the color when clicked
+        linkToRegister.setText(spannableString);
+        linkToRegister.setMovementMethod(LinkMovementMethod.getInstance());
 
     }
     private void initViewItem() {
