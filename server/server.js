27d057a3feed39b00c7c19ef5a119e9ad9108402
Daniel Cohen Shor <117212151+DanielCohenShor@users.noreply.github.com>
2023-06-19 17:02:27 +0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,61 +1,48 @@
 const express = require('express');
 const app = express();
-//the path of the server
 const path = require('path');
-
-// The library that allows using environment variables from config files
 const CustomEnv = require('custom-env');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const http = require('http');
+const { Server } = require('socket.io');
 
-// Define the environment variables
 CustomEnv.env(process.env.NODE_ENV, './config');
 
-const bodyParser = require('body-parser');
+// Set the maximum request size limit
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 
-const mongoose = require('mongoose');
 mongoose.connect(process.env.CONNECTION_STRING, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
 
-// Using CORS to allow Socket.IO communication on different ports
-const cors = require('cors');
 app.use(cors());
 
-// Create the HTTP server
-const http = require('http');
 const server = http.createServer(app);
-
-// Create the Socket.IO instance
-const { Server } = require('socket.io');
 const io = new Server(server, {
   cors: {
     origin: 'http://localhost:3000',
     methods: ['GET', 'POST', 'DELETE']
   }
 });
-// Export the io instance
 module.exports = { io };
 
-// Import and handle the sockets
 const socketHandler = require('./HandleSocketServer');
-
-//routes:
-app.use(express.static('public/build'));
-
 const users = require('./routes/Users');
-app.use('/api/Users', users);
-
 const tokens = require('./routes/Tokens');
-app.use('/api/Tokens', tokens);
-
 const chats = require('./routes/Chats');
-app.use('/api/Chats', chats);
-
-// handle request to the routes of the react
 const registerRouter = require('./routes/Register');
+
+app.use(express.static('public/build'));
+app.use('/api/Users', users);
+app.use('/api/Tokens', tokens);
+app.use('/api/Chats', chats);
 app.use('/', registerRouter);
 
-// Start the server
 server.listen(process.env.PORT);
